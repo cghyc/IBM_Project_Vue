45fd2ff2a8da0f1f6846f59901d91fdae4b34878
cghyc <13049617353@163.com>
2020-11-17 11:03:05 +0800

    add files

--- ibm_project_vue/src/main.js
+++ ibm_project_vue/src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//导入全局样式表
+import "./assets/css/global.css"
+
+import './plugins/element.js'
+
 Vue.config.productionTip = false;
 
 new Vue({
